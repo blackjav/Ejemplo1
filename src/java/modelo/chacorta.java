a47611e0e49e823b337ebc33a24b1ae59655cbed
javs <javs@javs>
2014-09-06 00:33:49 -0500

    agregamos

--- src/java/modelo/chacorta.java
+++ src/java/modelo/chacorta.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author javs
  */
 public class chacorta extends HttpServlet {
-
+        String sar;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
