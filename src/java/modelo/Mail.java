32224959fcbb2f94c2ac503ac9f0c46f0e22111c
javs <javs@javs>
2014-09-07 20:12:10 -0500

    Se cambio correro

--- src/java/modelo/Mail.java
+++ src/java/modelo/Mail.java
@@ -33,7 +33,7 @@ try {
     props.setProperty("mail.smtp.host", "smtp.gmail.com");//el segundo parametro se cambia deacuerdo a el smtp
     props.setProperty("mail.smtp.starttls.enable", "true");
     props.setProperty("mail.smtp.port", "587");
-    props.setProperty("mail.smtp.user", " javierhm810@gmail.com");
+    props.setProperty("mail.smtp.user", " revianmail@gmail.com");
     props.setProperty("mail.smtp.auth", "true");
 
     // Inicializar la Sesion la sesion
@@ -43,7 +43,7 @@ try {
     MimeMessage message = new MimeMessage(session);
     message.setFrom(new InternetAddress(" javierhm810@gmail.com"));
     message.addRecipient(Message.RecipientType.TO,new InternetAddress(correoDestinatario));
-    message.addRecipient(Message.RecipientType.BCC,new InternetAddress(" javierhm810@gmail.com"));
+    message.addRecipient(Message.RecipientType.BCC,new InternetAddress(" revianmail@gmail.com"));
     //CC A quien s ele envia una copia Oculta
     //BCC A quien s ele envia una copia Oculta
 
@@ -51,7 +51,7 @@ try {
     message.setText(textoCorreo);
     // envio MEnsaje.
     Transport trasporte = session.getTransport("smtp");
-    trasporte.connect("javierhm810@gmail.com","serafanprice2#");
+    trasporte.connect("revianmail@gmail.com","serafanprice2#");
     trasporte.sendMessage(message, message.getAllRecipients());
     // Cierre.
     trasporte.close();
@@ -61,15 +61,5 @@ try {
     
   }
     
-    
-    public static void main (String args[]){
-                Mail m = new Mail();
-                
-        try {
-            System.out.println("Ejecuntando");
-            m.enviarMail("ssw810@hotmail.com", "penes ", "penes");
-        } catch (MessagingException ex) {
-            Logger.getLogger(Mail.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+
 }
